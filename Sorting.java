55ad17631c7a2747efe250f4b29aacf2ff7d7e16
VIJAY GUPTA <50875814+VIjay-Nits@users.noreply.github.com>
2020-01-25 23:52:48 +0530

    quick sort Implementation added

--- Sorting.java
+++ Sorting.java
@@ -39,7 +39,33 @@ public class MySort {
         }
     }
     
-   
+    public void quickSort(int a[],int left,int right){
+        //left index from 0 and right= last index
+        if(left<right){
+            int pi_index=partition(a,left,right);
+            
+            quickSort(a,pi_index+1,right);//for left side of pivot
+            quickSort(a,left,pi_index-1);//for right side of pivot
+            
+        }
+        
+    }
+    private int partition(int a[],int left,int right){
+            //last element is used as pivot
+            int pivot=a[right];
+            int i=left-1;
+            for(int j=left;j<right;j++){
+                if(pivot>a[j]){
+                    i++;
+                    int temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+            int temp=a[i+1];
+            a[i+1]=a[right];
+            a[right]=temp;
+            return i+1;
     }
     
     
