568394ccd70c01690736d1fdfeea98e9cfb9d65b
VIJAY GUPTA <50875814+VIjay-Nits@users.noreply.github.com>
2020-01-26 01:24:39 +0530

    merge sort Implementation added

--- Sorting.java
+++ Sorting.java
@@ -50,6 +50,7 @@ public class MySort {
         }
         
     }
+    //used in quick sort
     private int partition(int a[],int left,int right){
             //last element is used as pivot
             int pivot=a[right];
@@ -66,6 +67,53 @@ public class MySort {
             a[i+1]=a[right];
             a[right]=temp;
             return i+1;
+    }
+    
+    public void mergeSort(int []a,int left,int right){
+        if(left<right){
+            int m=(left+right)/2;
+            mergeSort(a, left, m);
+            mergeSort(a, m+1, right);
+            merging(a,left,m,right);
+            
+        }
+        
+        
+    }
+    //used in merge sort
+    private void merging(int[]a,int left,int m,int right){
+        int n1=m-left+1;
+        int n2=right-(m+1)+1;
+        int []L=new int[n1];
+        int []R=new int[n2];
+        
+        for(int i=0;i<n1;i++){L[i]=a[left+i];}
+        for(int i=0;i<n2;i++){R[i]=a[m+1+i];}
+        
+        int i=0;int j=0;
+        int k=left;
+        while(i<n1&&j<n2){
+            if(L[i]<=R[j]){
+                a[k]=L[i];
+                i++;
+            }
+            else {
+                a[k]=R[j];
+                j++;
+            }
+            k++;
+        }
+        
+        while(i<n1){
+            a[k]=L[i];
+            i++;
+            k++;
+        }
+        while(j<n2){
+            a[k]=R[j];
+            j++;
+            k++;
+        }
     }
     
     
